4c3012a5ac62f64662615afd7cc18c435bb7fd9a
vishyvishal217 <46371201+vishyvishal217@users.noreply.github.com>
2020-10-22 01:30:23 +0530

    update 2

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello Vishal!"
+        message : "Hello Vishal !"
     }
 
     callback(null, result);
